ab594a62173f6aaa43a20b38ed4ce35b02979cec
Tural Mustafayev <mustafayevtural46@gmail.com>
2019-06-17 22:10:57 +0400

    fdsf

---
+++ SemaphoreThread/CreateThreadCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SemaphoreThread
+{
+    public class CreateThreadCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        ViewModel ViewModel;
+
+        public CreateThreadCommand(ViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SemaphoreThread/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreThread
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnNotifyPropertyChanged(string param)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(param));
+        }
+        public ObservableCollection<string> WorkingThreads { get; set; }
+        public ObservableCollection<string> WaitingThreads { get; set; }
+        public ObservableCollection<string> CreatedThreads { get; set; }
+        public static List<Thread> Threads { get; set; }
+        public static Semaphore semaphore = new Semaphore(2, 2);
+
+    }
+}
